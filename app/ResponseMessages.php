ed5d4e1813c30a27a38ad9b70e0f3e349cb1bda1
Deep Panchal <panchaldeep009@gmail.com>
2019-04-03 22:17:18 -0400

    New Respones message added

--- app/ResponseMessages.php
+++ app/ResponseMessages.php
@@ -11,7 +11,7 @@ class ResponseMessages {
             ),
             'no_space' => array(
                 'status' => 404,
-                'message' => "Parking Space not Found"
+                'message' => "Parking Space not available"
             ),
             'tech_error' => array(
                 'status' => 401,
@@ -25,6 +25,23 @@ class ResponseMessages {
             'success' => array(
                 'status' => 200, 
                 'message' => "Ticket Number Found"
+            ),
+            'no_ticket' => array(
+                'status' => 404,
+                'message' => "Ticket Number Not Found"
+            ),
+        );
+    }
+
+    public static function payForTicket () {
+        return (object) array(
+            'success' => array(
+                'status' => 200, 
+                'message' => "Payment successful"
+            ),
+            'tech_error' => array(
+                'status' => 501,
+                'message' => "Payment unsuccessful"
             ),
             'no_ticket' => array(
                 'status' => 404,
